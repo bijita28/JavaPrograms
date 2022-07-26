d6ae00dd28488c0631e65d101c26acb30772e337
bijita28 <bijita.k28@gmail.com>
2022-07-26 14:07:17 +0530

    commit 2

--- src/test1/StringRevConcept.java
+++ src/test1/StringRevConcept.java
@@ -4,6 +4,25 @@ public class StringRevConcept {
 
 	public static void main(String[] args) {
 		
+		//Reverse string without using reverse function
+		String str="BIJITA";
+		String rev="";
+		
+		int len=str.length();
+		
+		for(int i=len-1; i>=0; i--) {
+			rev=rev+str.charAt(i);
+		}
+		
+		System.out.println(rev);
+		
+		//Reverse a string using reverse function
+		
+		String str1="APPLE";
+		StringBuffer cb=new StringBuffer(str1);
+		
+		System.out.println(cb.reverse());
+		
 	}
 
 }
