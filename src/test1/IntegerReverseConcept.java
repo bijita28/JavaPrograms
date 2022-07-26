018d72624c0b2f6bb3ef2b2bdd14f7b6b35fd7ff
bijita28 <bijita.k28@gmail.com>
2022-07-26 14:16:57 +0530

    commit it

---
+++ src/test1/IntegerReverseConcept.java
@@ -0,0 +1,20 @@
+package test1;
+
+public class IntegerReverseConcept {
+
+	public static void main(String[] args) {
+		 
+		int num=12345;
+		int rev=0;
+		
+		while(num!=0) {
+		rev=rev*10+num%10;
+		num=num/10;
+		}
+		
+		System.out.println(rev);
+		
+
+	}
+
+}
